42330d43549ce510246b38971a545b76d517107d
jordan o'neal <joneal6694@gmail.com>
2020-10-06 20:16:26 -0600

    carousel

--- jordanportfolio/src/App.js
+++ jordanportfolio/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 // import './App.css';
 import MyNavbar from "./components/Navbar/MyNavbar";
+import MyCarousel from "./components/Carousel/MyCarousel";
+import Message from "./components/Message/Message";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
+      <MyCarousel />
+      <Message />
     </div>
   );
 }
